fd06e7cfa00ba3c30d921a221cfef6758f488564
AbinashSamal1 <abinash.samal@techriff.in>
2022-12-06 10:06:11 +0530

    routing done!

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
+import { CollectionsComponent } from './collections/collections.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'collection', component: CollectionsComponent },
 ];
 
 @NgModule({
